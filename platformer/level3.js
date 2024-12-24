0565b5421f8380b884ec223ade67cce816992f39
Ram10 <Rameezmajid10@outlook.com>
2024-12-24 21:18:54 +0000

    Update level3.js

--- platformer/level3.js
+++ platformer/level3.js
@@ -1,1 +1,11 @@
-
+var levels = [
+    {
+        platforms: [ // Level 1 platforms
+             { x: 0, y: 180, width: 400, height: 20 }, // Ground platform
+             { x: 300, y: 300, width: 100, height: 20 },
+             { x: 250, y: 100, width: 100, height: 20 },
+        ],
+        endObject:  { x: 400, y: 400, width: 100, height: 20 }, // End object for completion
+        completionURL: "https://szhu.github.io/3030/" // URL for level 1 completion
+    }
+];
