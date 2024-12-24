8efb67e8c42c2fc1be0d1956112b2a2390d4d14a
Ram10 <Rameezmajid10@outlook.com>
2024-12-24 20:33:13 +0000

    Create level2.js

---
+++ platformer/level2.js
@@ -0,0 +1,11 @@
+var levels = [
+    {
+        platforms: [ // Level 1 platforms
+           { x: 0, y: 180, width: 400, height: 20 }, // Ground platform
+                { x: 50, y: 150, width: 100, height: 20 },
+                { x: 250, y: 100, width: 100, height: 20 },
+        ],
+                { x: 150, y: 130, width: 50, height: 20 }
+        completionURL: "https://internetperson-dev.github.io/site/3ds2.html" // URL for level 1 completion
+    }
+];
