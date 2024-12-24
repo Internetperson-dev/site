7254ab30192ce902328ce6a2a5db91a138ba6ead
Ram10 <Rameezmajid10@outlook.com>
2024-12-24 20:17:14 +0000

    Create level1.js

---
+++ platformer/level1.js
@@ -0,0 +1,18 @@
+// Level definitions
+var levels = [
+    [ // Level 1
+        { x: 0, y: 180, width: 400, height: 20 }, // Ground platform
+        { x: 100, y: 160, width: 100, height: 20 },
+        { x: 200, y: 120, width: 100, height: 20 }
+    ],
+    [ // Level 2
+        { x: 0, y: 180, width: 400, height: 20 }, // Ground platform
+        { x: 50, y: 150, width: 100, height: 20 },
+        { x: 250, y: 100, width: 100, height: 20 },
+        { x: 150, y: 130, width: 50, height: 20 }
+    ]
+];
+
+// Export the levels if using modules
+// Uncomment the following line if using ES6 modules in the project
+// export { levels };
