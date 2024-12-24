2ccce9ab52a82123bb491fe7e3aac87a41bea456
Ram10 <Rameezmajid10@outlook.com>
2024-12-24 20:17:22 +0000

    Update game.js

--- platformer/game.js
+++ platformer/game.js
@@ -35,21 +35,6 @@ var endObject = {
     height: 30
 };
 
-// Levels definition
-var levels = [
-    [ // Level 1
-        { x: 0, y: 180, width: 400, height: 20 }, // Ground platform
-        { x: 100, y: 160, width: 100, height: 20 },
-        { x: 200, y: 120, width: 100, height: 20 }
-    ],
-    [ // Level 2
-        { x: 0, y: 180, width: 400, height: 20 }, // Ground platform
-        { x: 50, y: 150, width: 100, height: 20 },
-        { x: 250, y: 100, width: 100, height: 20 },
-        { x: 150, y: 130, width: 50, height: 20 }
-    ]
-];
-
 var currentLevel = 0; // Start with level 0
 var platforms = levels[currentLevel]; // Platforms for the current level
 
