768831cc452cecb54f3ffa840142dabbc5117857
Ram10 <Rameezmajid10@outlook.com>
2024-12-26 17:23:26 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,23 @@
 const CACHE_NAME = 'fortnite-pwa-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/icon-192x192.png',
-    '/icon-512x512.png',
+    'https://internetperson-dev.github.io/site/fornite/',
+    'https://internetperson-dev.github.io/site/fornite/index.html',
+    'https://internetperson-dev.github.io/site/icon-192x192.png',
+    'https://internetperson-dev.github.io/site/icon-512x512.png',
+    'https://internetperson-dev.github.io/site/fornite/style.css', // Include your CSS file if applicable
+    'https://internetperson-dev.github.io/site/fornite/script.js'  // Include your JS file if applicable
 ];
 
-// Install event
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log('Opened cache');
-            return cache.addAll(
-                urlsToCache.map(url => new Request(url, { cache: 'reload' }))
-            );
+            return cache.addAll(urlsToCache);
         }).catch(err => {
-            console.error('Caching failed:', err);
+            console.error('Failed to cache resources:', err);
         })
     );
 });
 
-// Fetch event
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
@@ -30,14 +26,12 @@ self.addEventListener('fetch', event => {
     );
 });
 
-// Activate event
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
                     if (cache !== CACHE_NAME) {
-                        console.log('Clearing old cache:', cache);
                         return caches.delete(cache);
                     }
                 })
