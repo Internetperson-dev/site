e3fa46f506aa7c423833273c87879427b37920f7
Ram10 <Rameezmajid10@outlook.com>
2025-03-14 16:39:30 +0000

    Update service-worker.js

--- etc/service-worker.js
+++ etc/service-worker.js
@@ -1,19 +1,8 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('v1').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/service-worker.js'
-            ]);
-        })
-    );
-});
+window.onload = () => {
+  'use strict';
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
-});
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
+  }
+}
